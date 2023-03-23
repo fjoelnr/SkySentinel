1148ff0ed0795767e5cccd3f50d3be04f7c443ae
fjoelnir <fjoelnir@thrudhamar.eu>
2023-03-23 21:47:02 +0100

    BME280 Sensor initial added

---
+++ sensors/bme280_sensor.cpp
@@ -0,0 +1,23 @@
+#include "bme280_sensor.h"
+
+BME280_Sensor::BME280_Sensor() {
+}
+
+bool BME280_Sensor::begin() {
+  if (!bme.begin(0x76)) {
+    return false;
+  }
+  return true;
+}
+
+float BME280_Sensor::readTemperature() {
+  return bme.readTemperature();
+}
+
+float BME280_Sensor::readHumidity() {
+  return bme.readHumidity();
+}
+
+float BME280_Sensor::readPressure() {
+  return bme.readPressure() / 100.0F;
+}
