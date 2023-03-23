2f887322104d69d1acd310b6a23280af4373582c
fjoelnir <fjoelnir@thrudhamar.eu>
2023-03-23 22:16:49 +0100

    First working example for BME280 Sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
+#include "sensors/bme280_sensor.h"
+
+BME280_Sensor bmeSensor;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  if (!bmeSensor.begin()) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.print("Temperature = ");
+  Serial.print(bmeSensor.readTemperature());
+  Serial.println(" *C");
+
+  Serial.print("Humidity = ");
+  Serial.print(bmeSensor.readHumidity());
+  Serial.println(" %");
+
+  Serial.print("Pressure = ");
+  Serial.print(bmeSensor.readPressure());
+  Serial.println(" hPa");
+
+  delay(2000);
 }
