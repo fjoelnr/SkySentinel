fa167cf66a04eae3520e04bb0b9e4518a07e1350
Fjölnir Óðalriksson <31723624+fjoelnr@users.noreply.github.com>
2023-03-28 10:58:05 +0200

    Create credentials_template.h

---
+++ src/credentials_template.h
@@ -0,0 +1,14 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// Replace with your actual Wi-Fi credentials
+const char* WIFI_SSID = "your_wifi_ssid";
+const char* WIFI_PASSWORD = "your_wifi_password";
+
+// Replace with your actual MQTT credentials
+const char* MQTT_SERVER = "your_mqtt_server";
+const int MQTT_PORT = 1883;
+const char* MQTT_USER = "your_mqtt_user";
+const char* MQTT_PASSWORD = "your_mqtt_password";
+
+#endif // CREDENTIALS_H
