d5284c05b689d00ffeb529fe8e1afcb028e1fcc9
Fjölnir Óðalriksson <31723624+fjoelnr@users.noreply.github.com>
2023-03-28 12:33:06 +0200

    Create credentials_example.h

---
+++ include/credentials_example.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define WIFI_SSID "your_ssid"
+#define WIFI_PASSWORD "your_password"
+#define MQTT_SERVER "your_mqtt_server"
+#define MQTT_PORT 1883
+#define MQTT_USER "your_mqtt_user"
+#define MQTT_PASSWORD "your_mqtt_password"
