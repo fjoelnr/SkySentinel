3c98a1778ffde70d7b749117f8f6def5e71d50aa
fjoelnr <andre@thrudhamar.eu>
2023-03-28 18:07:11 +0200

    implemented Autotest and autoformat

--- src/credentials_template.h
+++ src/credentials_template.h
@@ -1,14 +1,16 @@
 #ifndef CREDENTIALS_H
 #define CREDENTIALS_H
 
+// Wi-Fi Credentials
 // Replace with your actual Wi-Fi credentials
-const char* WIFI_SSID = "your_wifi_ssid";
-const char* WIFI_PASSWORD = "your_wifi_password";
+#define WIFI_SSID "your_wifi_ssid"
+#define WIFI_PASSWORD "your_wifi_password"
 
+// MQTT Credentials
 // Replace with your actual MQTT credentials
-const char* MQTT_SERVER = "your_mqtt_server";
-const int MQTT_PORT = 1883;
-const char* MQTT_USER = "your_mqtt_user";
-const char* MQTT_PASSWORD = "your_mqtt_password";
+#define MQTT_SERVER "your_mqtt_server"
+#define MQTT_PORT 1883 // Change if your MQTT server uses a different port
+#define MQTT_USERNAME "your_mqtt_user"
+#define MQTT_PASSWORD "your_mqtt_password"
 
 #endif // CREDENTIALS_H
