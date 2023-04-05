539cefb1e5c58ce78c4d3b3c55cf0e736b50826c
Fjölnir Óðalriksson <31723624+fjoelnr@users.noreply.github.com>
2023-04-05 08:32:17 +0200

    Update credentials_template.h

--- src/credentials_template.h
+++ src/credentials_template.h
@@ -1,16 +1,48 @@
+/**
+ * @file credentials.h
+ * @author fjoelnr
+ * @date 2023-04-04
+ * @brief This file contains the Wi-Fi and MQTT credentials.
+ */
+
 #ifndef CREDENTIALS_H
 #define CREDENTIALS_H
 
 // Wi-Fi Credentials
 // Replace with your actual Wi-Fi credentials
+
+/**
+ * @brief The Wi-Fi SSID to connect to.
+ */
 #define WIFI_SSID "your_wifi_ssid"
+
+/**
+ * @brief The Wi-Fi password for the specified SSID.
+ */
 #define WIFI_PASSWORD "your_wifi_password"
 
 // MQTT Credentials
 // Replace with your actual MQTT credentials
+
+/**
+ * @brief The MQTT server address.
+ */
 #define MQTT_SERVER "your_mqtt_server"
-#define MQTT_PORT 1883 // Change if your MQTT server uses a different port
+
+/**
+ * @brief The MQTT server port.
+ * @note Change if your MQTT server uses a different port.
+ */
+#define MQTT_PORT 1883
+
+/**
+ * @brief The MQTT username for authentication.
+ */
 #define MQTT_USERNAME "your_mqtt_user"
+
+/**
+ * @brief The MQTT password for the specified username.
+ */
 #define MQTT_PASSWORD "your_mqtt_password"
 
 #endif // CREDENTIALS_H
