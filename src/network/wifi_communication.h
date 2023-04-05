cff3163182aa32e495f038a06d4da319a5facca9
Fjölnir Óðalriksson <31723624+fjoelnr@users.noreply.github.com>
2023-04-05 08:39:15 +0200

    Update wifi_communication.h

--- src/network/wifi_communication.h
+++ src/network/wifi_communication.h
@@ -1,3 +1,10 @@
+/**
+ * @file wifi_communication.h
+ * @author fjoelnr
+ * @date 2023-04-04
+ * @brief This file contains the definition of the WifiCommunication class for managing Wi-Fi connectivity.
+ */
+
 #ifndef WIFI_COMMUNICATION_H
 #define WIFI_COMMUNICATION_H
 
@@ -5,10 +12,26 @@
 
 class WifiCommunication {
 public:
+  /**
+   * @brief Construct a new WifiCommunication object.
+   * @param ssid The Wi-Fi SSID.
+   * @param password The Wi-Fi password.
+   */
   WifiCommunication(const char* ssid, const char* password);
 
+  /**
+   * @brief Set up the Wi-Fi module.
+   */
   void setup();
+
+  /**
+   * @brief Connect to the Wi-Fi network.
+   */
   void connect();
+
+  /**
+   * @brief Disconnect from the Wi-Fi network.
+   */
   void disconnect();
 
 private:
