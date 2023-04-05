4661bbf131ce635e655ddb513e6abd90087dd2f6
Fjölnir Óðalriksson <31723624+fjoelnr@users.noreply.github.com>
2023-04-05 08:41:02 +0200

    Update wifi_communication.cpp

--- src/network/wifi_communication.cpp
+++ src/network/wifi_communication.cpp
@@ -1,12 +1,30 @@
+/**
+ * @file wifi_communication.cpp
+ * @author fjoelnr
+ * @date 2023-04-04
+ * @brief This file contains the implementation of the WifiCommunication class for managing Wi-Fi connectivity.
+ */
+
 #include "wifi_communication.h"
 
+/**
+ * @brief Construct a new WifiCommunication object.
+ * @param ssid The Wi-Fi SSID.
+ * @param password The Wi-Fi password.
+ */
 WifiCommunication::WifiCommunication(const char* ssid, const char* password)
     : ssid(ssid), password(password) {}
 
+/**
+ * @brief Set up the Wi-Fi module.
+ */
 void WifiCommunication::setup() {
   WiFi.mode(WIFI_STA);
 }
 
+/**
+ * @brief Connect to the Wi-Fi network.
+ */
 void WifiCommunication::connect() {
   if (WiFi.status() != WL_CONNECTED) {
     Serial.print("Connecting to " + String(ssid) + " ");
@@ -21,6 +39,9 @@ void WifiCommunication::connect() {
   }
 }
 
+/**
+ * @brief Disconnect from the Wi-Fi network.
+ */
 void WifiCommunication::disconnect() {
   if (WiFi.status() == WL_CONNECTED) {
     WiFi.disconnect();
